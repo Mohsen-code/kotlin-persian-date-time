a6eb65177af34ac929145b28379711e6cbca073f
Mohsen Falahnejad <rojaweb1@gmail.com>
2022-02-04 20:06:58 +0330

    persian months enum added

---
+++ src/jvmMain/kotlin/mohsen/coder/PersianMonths.kt
@@ -0,0 +1,16 @@
+package mohsen.coder
+
+enum class PersianMonths {
+    Farvardin,
+    Ordibehesht,
+    Khordad,
+    Tir,
+    Mordad,
+    Shahrivar,
+    Mehr,
+    Aban,
+    Azar,
+    Dey,
+    Bahman,
+    Esfand
+}
